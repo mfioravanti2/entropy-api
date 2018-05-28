0253f8aa91b7038c203d19091350314c1ad721b2
Mark Fioravanti II <tegzilla@ecultist.com>
2018-05-28 15:23:18 -0400

    Coverted logs to json format.

--- command/server/logging.go
+++ command/server/logging.go
@@ -4,14 +4,30 @@ import (
 	"net/http"
 	"time"
 	"log"
+	"encoding/json"
+	"fmt"
 )
+
+type LogEntry struct {
+	Time time.Time `json:"time"`
+	Method string `json:"method"`
+	URI string `json:"uri"`
+	Name string `json:"name"`
+}
 
 func Logger(inner http.Handler, name string) http.Handler {
 	return http.HandlerFunc( func( w http.ResponseWriter, r *http.Request){
-		start := time.Now()
+		var logEntry LogEntry
+		logEntry = LogEntry{time.Now(), r.Method, r.RequestURI, name}
+
+		entryJson, err := json.Marshal(logEntry)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		log.Printf( "%s", string(entryJson))
 
 		inner.ServeHTTP(w,r)
-
-		log.Printf( "%s\t%s\t%s\t%s", time.Since(start), r.Method, r.RequestURI, name)
 	})
 }
