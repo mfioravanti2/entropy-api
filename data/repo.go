a4ad43fde9080fc990a113e0e44fb0b4ff793644
Mark Fioravanti II <tegzilla@ecultist.com>
2018-05-28 15:23:37 -0400

    Wired country listings.

--- data/repo.go
+++ data/repo.go
@@ -1,16 +1,34 @@
 package data
 
 import (
+	"io/ioutil"
 	"github.com/mfioravanti2/entropy-api/model"
+	"encoding/json"
 )
 
+// var countries map[string]string
+var countries model.Countries
+
 func init() {
+	jsonData, err := ioutil.ReadFile("data/sources/sources.json")
+	if err != nil {
+		panic(err)
+	}
+
+	err = json.Unmarshal(jsonData, &countries)
+	if err != nil {
+		panic(err)
+	}
 }
 
-func GetCountries() model.Countries {
-	var countries model.Countries
+func GetCountries() []string {
+	var names []string
 
-	return countries
+	for _, country := range countries {
+		names = append(names, country.Name)
+	}
+
+	return names
 }
 
 func GetAttributes( country string ) model.Attributes {
